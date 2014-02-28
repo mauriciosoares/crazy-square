3f85d42eea58b1eb6b25daa43620fa751a707693
Mauricio Soares <m.soares2603@gmail.com>
2014-02-28 14:01:13 -0300

    Add dynamicaly generated hero

--- js/play.js
+++ js/play.js
@@ -12,12 +12,23 @@ gameStates.Play.prototype = {
         // add floor as a group
         this.floor = this.game.add.group();
 
+        // configs hero
+        this.hero = this.game.add.sprite(-50, -50, 'hero');
+        this.hero.body.gravity.y = 1000;
+
+        this.initHero();
+
         // Draws the map
         this.drawMap(stages.maps[stages.current]);
     },
 
     update: function() {
+        this.game.physics.collide(this.hero, this.floor);
+    },
 
+    initHero: function() {
+        this.hero.x = stages.maps[stages.current].heroPosition.x;
+        this.hero.y = stages.maps[stages.current].heroPosition.y;
     },
 
     drawMap: function(map) {
@@ -31,11 +42,15 @@ gameStates.Play.prototype = {
 
             var xLength = map.matrix[y].length;
             for(x = 0; x < xLength; x += 1) {
-
+                var floor;
                 if(map.matrix[y][x] == 1) {
-                    this.floor.create(x * 30, y * 30, 'floor');
+                     floor = this.floor.create(x * 30, y * 30, 'floor');
+                     console.log(floor);
+                     floor.body.immovable = true;
                 }
             }
         }
+
+        this.initHero();
     }
 };
