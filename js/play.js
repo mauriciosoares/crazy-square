00d773770115d0760374dd202d165a919e2c88a6
Mauricio Soares <m.soares2603@gmail.com>
2014-03-06 10:06:34 -0300

    Add something I dont remember

--- js/play.js
+++ js/play.js
@@ -6,9 +6,6 @@ gameStates.Play = function() {
 
 gameStates.Play.prototype = {
     create: function() {
-        // Add Hero into the game;
-        // this.game.add.sprite(20, 20, 'hero');
-
         // add floor as a group
         this.floor = this.game.add.group();
 
@@ -16,17 +13,40 @@ gameStates.Play.prototype = {
         this.hero = this.game.add.sprite(-50, -50, 'hero');
         this.hero.body.gravity.y = 1000;
 
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jumpHero, this);
+
         // Draws the map
         this.drawMap(stages.maps[stages.current]);
+
+        // some conditions
+        this.direction = 200;
+        console.log(this.hero.body.touching);
     },
 
     update: function() {
-        this.game.physics.collide(this.hero, this.floor);
+        this.game.physics.collide(this.hero, this.floor, this.invertDirection, null, this);
+
+        this.hero.body.velocity.x = this.direction;
+    },
+
+    invertDirection: function(hero, floor) {
+        // invert direction according to touch event
+        if(hero.body.touching.right) {
+            // makes hero go to left
+            this.direction = -200;
+        }else if(hero.body.touching.left) {
+            this.direction = 200;
+        }
     },
 
     initHero: function() {
         this.hero.x = stages.maps[stages.current].heroPosition.x;
         this.hero.y = stages.maps[stages.current].heroPosition.y;
+    },
+
+    jumpHero: function() {
+        this.hero.body.velocity.y = -430;
     },
 
     drawMap: function(map) {
